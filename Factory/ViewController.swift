05093c03c5e0c4e25a9eb80a88e2fbf75705ec57
Nataliya Lazouskaya <93728977+NataliyaLaz@users.noreply.github.com>
2022-10-17 15:10:45 +0300

    Initial Commit

---
+++ Factory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Factory
+//
+//  Created by Nataliya Lazouskaya on 17.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
